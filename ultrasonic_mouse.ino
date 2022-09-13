2e23a7d136a2c8f2d8fddc9200a55c13657ad218
Casey Boswell <34087449+caseyboswell@users.noreply.github.com>
2022-09-13 12:41:14 +1000

    attempt add front sensor

--- ultrasonic_mouse.ino
+++ ultrasonic_mouse.ino
@@ -2,24 +2,32 @@
 //2016.12.08
 
 // ON/OFF Switch
-const int buttonPin = 8;    // the number of the pushbutton pin
+const int buttonPin = 9;    // the number of the pushbutton pin
 bool ON = false;         // the current state of the output pin (initially OFF)
 int buttonState;             // the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
-// Ultrasonic Sensor
+// Ultrasonic Sensor Library
 #include "SR04.h"
-#define TRIG_PIN 12
-#define ECHO_PIN 11
-SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-long distance;
+
+// Back Ultrasonic Sensor
+#define BACK_TRIG_PIN 13
+#define BACK_ECHO_PIN 12
+SR04 back_sensor = SR04(BACK_ECHO_PIN, BACK_TRIG_PIN);
+long back_distance;
+
+// Front Ultrasonic Sensor
+#define FRONT_TRIG_PIN 3
+#define FRONT_ECHO_PIN 2
+SR04 front_sensor = SR04(FRONT_ECHO_PIN, FRONT_TRIG_PIN);
+long front_distance;
 
 // DC Motor
-#define ENABLE 5
-#define DIRA 3
-#define DIRB 4
+#define ENABLE 7
+#define DIRA 6
+#define DIRB 5
 int i;
 
 void setup() {
@@ -45,13 +53,16 @@ void loop() {
   if (ON) {
     
     // Collect & record distance measure
-    distance = sr04.Distance();
-//    Serial.print(a);
+    back_distance = back_sensor.Distance();
+    front_distance = front_sensor.Distance();
+    Serial.print(back_distance);
+    Serial.println("cm");
+//    Serial.print("front = " + front_distance);
 //    Serial.println("cm");
-//    delay(10);
+    delay(10);
 
     // Turn on motor if closer than 10cm
-    if (distance < 10) {
+    if (back_distance < 10) {
       // Turn the motor on
       digitalWrite(ENABLE,255); // enable full speed
       digitalWrite(DIRA,HIGH); //one way
